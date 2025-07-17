fdd7bde478b69827c601539d3a966a04211927e6
alok <alok@samyotech.com>
2025-07-17 07:53:12 +0530

    code for sell

--- src/bot/buy.js
+++ src/bot/buy.js
@@ -240,7 +240,7 @@ const startBotForBuy = async () => {
           symbolObject?.price
         );
         if (symbolObject?.status == false) {
-          if (symbolObject?.price > symbolObject?.trades?.[0]?.buyingAmount) {
+          if (symbolObject?.price > symbolObject?.trades?.[0]?.buyingAmount?.$numberDecimal) {
             console.log(
               `order sell   quantity : ${symbolObject?.trades?.[0]?.quantity} symbol:  ${symbolObject?.symbol} @ ${symbolObject?.price} `
             );
