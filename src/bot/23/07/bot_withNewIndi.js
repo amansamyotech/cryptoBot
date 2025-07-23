251e579a3eb4c65727f1c9f3e57994c3986b21db
alok <alok@samyotech.com>
2025-07-23 12:42:25 +0530

    code

--- src/bot/23/07/bot_withNewIndi.js
+++ src/bot/23/07/bot_withNewIndi.js
@@ -420,7 +420,7 @@ setInterval(async () => {
       console.error(`Error with ${sym}:`, err);
     }
   }
-}, 60 * 3000); // Run every 5 minute
+}, 60 * 1000); // Run every 5 minute
 
 async function checkOrders(symbol) {
   try {
