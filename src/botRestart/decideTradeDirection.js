4198a6a1f81fe08910c2575bc920f296bef262c5
alok <alok@samyotech.com>
2025-07-30 12:48:14 +0530

    rebuild bot with new logic

--- src/botRestart/decideTradeDirection.js
+++ src/botRestart/decideTradeDirection.js
@@ -8,7 +8,7 @@ const binance = new Binance().options({
   test: false,
 });
 
-const TIMEFRAME_MAIN = "5m";
+const TIMEFRAME_MAIN = "1m";
 const TIMEFRAME_TREND = "15m";
 const EMA_ANGLE_THRESHOLD = 30;
 
