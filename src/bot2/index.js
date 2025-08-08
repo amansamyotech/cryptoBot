bef28d7d043b8a4ac0371c5fee96e118c77d49e8
alok <alok@samyotech.com>
2025-08-08 15:13:02 +0530

    code

--- src/bot2/index.js
+++ src/bot2/index.js
@@ -565,6 +565,8 @@ async function placeShortOrder(symbol, marginAmount) {
 
 async function processSymbol(symbol, maxSpendPerTrade) {
   const decision = await decideTradeDirection(symbol);
+  console.log(`decision`,decision);
+  
   if (decision === "LONG") {
     await placeBuyOrder(symbol, maxSpendPerTrade);
   } else if (decision === "SHORT") {
