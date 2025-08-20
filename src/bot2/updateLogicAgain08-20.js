ebcbc824a97610e1b2a0399a8fd1391fc250435c
alok <alok@samyotech.com>
2025-08-20 17:10:26 +0530

    code

--- src/bot2/updateLogicAgain08-20.js
+++ src/bot2/updateLogicAgain08-20.js
@@ -513,7 +513,7 @@ setInterval(async () => {
   } else {
     console.log("not enough amount");
   }
-}, 2000);
+}, 3000);
 
 // Profit management and exit monitoring interval - INCREASED FREQUENCY FOR REAL-TIME ROI MONITORING
 setInterval(async () => {
