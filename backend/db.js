d998c2bcef3ae0f1fe54a6e26e2152351dcfd1ae
alok <alok@samyotech.com>
2025-08-25 15:19:27 +0530

    inital commit

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://dsamyotech:RshvPx3EyS51nOEr@cluster0.wp1itfp.mongodb.net/nbitAi?retryWrites=true&w=majority&appName=Cluster0",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    "mongodb+srv://dsamyotech:RshvPx3EyS51nOEr@cluster0.wp1itfp.mongodb.net/nbitAi?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("✅ MongoDB connected successfully.");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
 
 module.exports = mongoose;
