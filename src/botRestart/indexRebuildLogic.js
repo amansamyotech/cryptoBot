3e541593e8260c48ae3415eea9b8d9c331fb3b77
alok <alok@samyotech.com>
2025-09-16 16:04:07 +0530

    code

--- src/botRestart/indexRebuildLogic.js
+++ src/botRestart/indexRebuildLogic.js
@@ -33,8 +33,8 @@ async function getUsdtBalance() {
 
 const LEVERAGE = 3;
 const ATR_LENGTH = 14;
-const ATR_MULTIPLIER_SL = 1.0;
-const ATR_MULTIPLIER_TP = 2.0;
+const ATR_MULTIPLIER_SL = 2.0;
+const ATR_MULTIPLIER_TP = 3.0;
 
 function calculateATR(candles, length = ATR_LENGTH) {
   if (candles.length < length + 1) return null;
