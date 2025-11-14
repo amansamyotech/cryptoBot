e01009d6cffd88181c5bdc1f265ae9140f1441d5
alok <alok@samyotech.com>
2025-11-14 12:37:09 +0530

    update streatgy and match from the orignal one

--- src/binance-bot/index.js
+++ src/binance-bot/index.js
@@ -37,8 +37,8 @@ async function getUsdtBalance() {
 
 const LEVERAGE = 3;
 const ATR_LENGTH = 14;
-const ATR_MULTIPLIER_SL = 2;
-const ATR_MULTIPLIER_TP = 2;
+const ATR_MULTIPLIER_SL = 1.5;
+const ATR_MULTIPLIER_TP = 1.5;
 
 function calculateATR(candles, length = ATR_LENGTH) {
   if (candles.length < length + 1) return null;
