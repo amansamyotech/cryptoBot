cd4c74962c3bcef7549e3da5a520baa40e292743
Aman Asati <amansamyotech@gmail.com>
2025-11-20 14:42:18 +0530

    code

--- bot2/index.js
+++ bot2/index.js
@@ -12,17 +12,6 @@ const mongoose = require("../backend/db.js");
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection is open!");
 });
-const isProcessing = {};
-const binance = new Binance().options({
-  APIKEY:
-    process.env.BINANCE_APIKEY ||
-    "0kB82SnxRkon7oDJqmCPykl4ar0afRYrScffMnRA3kTR8Qfq986IBwjqNA7fIauI",
-  APISECRET:
-    process.env.BINANCE_SECRETKEY ||
-    "6TWxLtkLDaCfDh4j4YcLa2WLS99zkZtaQjJnsAeGAtixHIDXjPdJAta5BJxNWrZV",
-  useServerTime: true,
-  test: false,
-});
 
 
 const ENVUSERID = process.env.USER_ID || "689c48ecdbd3da869cb3e0c5";
